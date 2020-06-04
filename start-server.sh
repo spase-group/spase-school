f0fca7a8b400b2a84cac978fdc090590bac1c2c6
T. King <tking@example.com>
2020-06-04 12:44:24 -0700

    Regenerate pages.

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,5 @@
 echo $PWD
 docker run --rm \
   -p 80:80 \
-  --mount type=bind,src="$PWD/docs",dst="/usr/share/nginx/html/spase-school" \
+  --mount type=bind,src="$PWD/docs",dst="/usr/share/nginx/html" \
   -it nginx 
